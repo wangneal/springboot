7d5dbbf6e39b660c5ca7f4e23adc9d2ce11eb3bf
wangneal <498336692@qq.com>
2018-07-24 23:15:16 +0800

    springboot+Shiro

---
+++ springboot-shiro/src/main/java/com/bcfou/springbootshiro/config/ShiroRealm.java
@@ -0,0 +1,48 @@
+package com.bcfou.springbootshiro.config;
+
+
+import com.bcfou.springbootshiro.mapper.UserMapper;
+import com.bcfou.springbootshiro.pojp.User;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ShiroRealm extends AuthorizingRealm {
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 获取用户角色和权限
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        // 获取用户输入的用户名和密码
+        String userName = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+
+        System.out.println("用户" + userName + "认证-----ShiroRealm.doGetAuthenticationInfo");
+        // 通过用户名到数据库查询用户信息
+        User user = userMapper.findByUsername(userName);
+
+
+
+        if (user == null) {
+            throw new UnknownAccountException("用户名或密码错误！");
+        }
+        if (!password.equals(user.getPassword())) {
+            throw new IncorrectCredentialsException("用户名或密码错误！");
+        }
+        if (user.getStatus().equals("0")) {
+            throw new LockedAccountException("账号已被锁定,请联系管理员！");
+        }
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+        return info;
+    }
+}
---
+++ springboot-shiro/src/main/java/com/bcfou/springbootshiro/pojp/User.java
@@ -0,0 +1,61 @@
+package com.bcfou.springbootshiro.pojp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private Date create_time;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
